ad3e91b98df565d0f0d1a93c34edb0278e5ffbe8
whiteprogramming <78683443+whiteprogramming@users.noreply.github.com>
2021-02-07 14:54:22 +0300

    C# Öğrencinin 3 Yazılı Not Ortalamasını Hesaplama

---
+++ C# Öğrencinin 3 Yazılı Not Ortalamasını Hesaplayan Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Not1, Not2, Not3,Ortalama;
+            Console.Write("\n\n Birinci Notu Girin :");
+            Not1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n\n İkinci Notu Girin :");
+            Not2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n\n Üçüncü Notu Girin :");
+            Not3 = Convert.ToInt32(Console.ReadLine());
+            Ortalama = (Not1 + Not2 + Not3) / 3;
+            Console.WriteLine("\n\n Öğrenci Ortalaması: " + Ortalama.ToString());
+            Console.ReadLine();
+
+
+        }
+    }
+}
