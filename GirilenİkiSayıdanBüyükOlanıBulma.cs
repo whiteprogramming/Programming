33fe858e5049c7f046bc0b9762d4ef6bd1f6ed81
whiteprogramming <78683443+whiteprogramming@users.noreply.github.com>
2021-02-10 15:06:06 +0300

    Girilen İki Sayıdan Büyük Olanı Bulma

---
+++ GirilenİkiSayıdanBüyükOlanıBulma.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Sayi1, Sayi2;
+            Console.Write("\n\n Birinci Sayıyı Girin :");
+            Sayi1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n\n İkinci Sayıyı Girin :");
+            Sayi2 = Convert.ToInt32(Console.ReadLine());
+            if (Sayi1 > Sayi2)
+            {
+                Console.WriteLine(Sayi1.ToString() + " , " + Sayi2.ToString() + "'den büyüktür.");
+            }
+            else if (Sayi1 < Sayi2)
+            {
+                Console.WriteLine(Sayi2.ToString() + " , " + Sayi1.ToString() + "'den büyüktür.");
+            }
+            else
+            {
+                Console.WriteLine("İki Sayı Eşittir.");
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
